885dd56bb051e4b50a54ecd369608234049b7246
SwatiMishra8517 <57552806+SwatiMishra8517@users.noreply.github.com>
2020-08-24 22:53:49 +0530

    assignment3rd-24th aug-todo app

---
+++ Assignment3/TODOAPP/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList";
+
+
+var destiny = document.querySelector("#container");
+
+ReactDOM.render (
+    <div>
+     
+     <TodoList/>
+    </div>,
+    destiny
+
+);
